385b89a48f3231ea4ae2c6f4665edad5813eaf69
TalalAhmed311 <talalahmedqurashi123@gmail.com>
2026-01-16 15:38:58 +0500

    add deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-sudo tee /etc/systemd/system/audio-transcriber.service >/dev/null <<'EOF'
+sudo tee /etc/systemd/system/audio-to-transcripts.service >/dev/null <<'EOF'
 [Unit]
-Description=Audio Transcriber (Streamlit)
+Description=Audio to Transcripts (Streamlit)
 After=network-online.target
 Wants=network-online.target
 
 [Service]
 User=ubuntu
-WorkingDirectory=/home/ubuntu/apps/audio-transcriber/AudiTranscriber
-ExecStart=/home/ubuntu/apps/audio-transcriber/AudiTranscriber/venv/bin/streamlit run /home/ubuntu/apps/audio-transcriber/app.py --server.address=0.0.0.0 --server.port=8503 --server.headless=true --server.maxUploadSize=200
+WorkingDirectory=/home/ubuntu/apps/audio-to-transcripts/
+ExecStart=/home/ubuntu/apps/audio-to-transcripts/venv/bin/streamlit run /home/ubuntu/apps/audio-to-transcripts/app.py --server.address=0.0.0.0 --server.port=8503 --server.headless=true --server.maxUploadSize=200
 Restart=always
 RestartSec=5
 Environment=PYTHONUNBUFFERED=1
@@ -17,5 +17,7 @@ WantedBy=multi-user.target
 EOF
 
 sudo systemctl daemon-reload
-sudo systemctl enable --now audio-transcriber
-sudo systemctl status audio-transcriber --no-pager
+sudo systemctl enable --now audio-to-transcripts
+sudo systemctl status audio-to-transcripts --no-pager
+# optional: tail logs
+sudo journalctl -u audio-to-transcripts -e --no-pager
